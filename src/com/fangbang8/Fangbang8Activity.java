a2226e2ea4ddad8d8d3fb4dd64d38a494937d892
Guan.L.Y <mrguanlingyu@gmail.com>
2012-06-12 23:51:23 +0800

    first commit

---
+++ src/com/fangbang8/Fangbang8Activity.java
@@ -0,0 +1,13 @@
+package com.fangbang8;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Fangbang8Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
